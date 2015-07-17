55bc6b718f3493e197d9963101dc4c43e4b37c11
Mike Jones <mike@westerncapelabs.com>
2015-07-17 18:01:03 +0200

    Fix travis settings

--- mama_ng_contentstore/testsettings.py
+++ mama_ng_contentstore/testsettings.py
@@ -1,10 +1,5 @@
 from mama_ng_contentstore.settings import *  # flake8: noqa
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://postgres:@localhost/mama_ng_contentstore'),
-}
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'TESTSEKRET'
 
